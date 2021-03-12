6049c3fc9585f11cc07e44011cf7c869fa9debc4
daniel <danielchungara1@gmail.com>
2021-03-12 15:58:05 -0300

    Translate messages into services.

--- src/services/resetPasswordService.js
+++ src/services/resetPasswordService.js
@@ -10,7 +10,7 @@ export const resetStep1 = async (email) => {
         return {'ok': true, 'message': data.message, 'data': data.data};
 
     } catch ({response}) {
-        let message = 'Error al cambiar la contraseña.';
+        let message = 'Error al recuperar contraseña.';
         if (response?.status === 409) {
             message = response.data.message;
         }
@@ -29,7 +29,7 @@ export const resetStep2 = async (email, code, password) => {
         return {'ok': true, 'message': data.message, 'data': data.data};
 
     } catch ({response}) {
-        let message = 'Error al cambiar la contraseña.';
+        let message = 'Error al cambiar contraseña.';
         if (response?.status === 409) {
             message = response.data.message;
         }
