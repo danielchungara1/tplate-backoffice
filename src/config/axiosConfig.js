b9d5b85fd1f7588be0d5fdefac44b87e51ca7186
daniel <danielchungara1@gmail.com>
2021-03-14 03:43:24 -0300

    Fix axios.

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosClient = axios.create({
     //LOCAL
-    baseURL: 'https://localhost:8080/api',
+    baseURL: 'http://localhost:8080/api',
 
     //PRODUCCION
     //baseURL: 'https://tplate.herokuapp.com/api',
